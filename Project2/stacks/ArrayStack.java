9d2f105e80ce41bc801ce05f3e54934a5a18c386
Brianna Shade <jarvis.brianna@gmail.com>
2014-05-04 15:15:45 -0700

    Array implementation

--- Project2/stacks/ArrayStack.java
+++ Project2/stacks/ArrayStack.java
@@ -8,14 +8,46 @@
  */
 package stacks;
 
-public class ArrayStack {
+import java.lang.reflect.Array;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Hello world!");
+public class ArrayStack<type> {
+
+	private type[] astack;
+	private int size, head;
+	
+	/** Constructor **/
+	@SuppressWarnings("unchecked")
+	public ArrayStack(int s){
+		size = s;
+		astack = (type[]) Array.newInstance(Object[].class, size);
+		head = size;
+	}
+	
+	/** Modifiers 
+	 * @throws Exception **/
+	public void Push(type o) throws Exception{
+		if(head < 1) throw new Exception("Cannot push full stack");
+		
+		astack[head--] = o;
+	}
+	public type Pop() throws Exception{
+		if(head > size-1) throw new Exception("Cannot pop empty stack");
+		
+		type element = astack[head];
+		astack[head] = null;
+		head++;
+		return element;
+	}
+	
+	/** Accessors **/
+	public type Peek(){
+		return astack[head];
+	}
+	public int Size(){
+		return size - head;
+	}
+	public boolean isEmpty(){
+		return head == size;
 	}
 
 }
