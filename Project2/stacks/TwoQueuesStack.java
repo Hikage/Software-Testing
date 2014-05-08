e3ca01ce40966873634ead4d68ee9d06d90517e9
Brianna Shade <jarvis.brianna@gmail.com>
2014-05-08 06:46:08 -0700

    TwoQueues implementation

--- Project2/stacks/TwoQueuesStack.java
+++ Project2/stacks/TwoQueuesStack.java
@@ -8,14 +8,50 @@
  */
 package stacks;
 
-public class TwoQueuesStack {
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.LinkedList;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class TwoQueuesStack<type> implements BaseStack<type>{
 
-	}
+	private ArrayList<Queue<type>> twoqueues;
+    private int activeQueue, inactiveQueue;
+
+    /** Constructor **/
+    public TwoQueuesStack(){
+    	twoqueues = new ArrayList<Queue<type>>();
+    	twoqueues.add(new LinkedList<type>());
+    	twoqueues.add(new LinkedList<type>());
+    	activeQueue = 0;
+    	inactiveQueue = 1;
+    }
+
+    /** Modifiers **/
+    public void Push(type o){
+    	twoqueues.get(inactiveQueue).add(o);
+    	twoqueues.get(inactiveQueue).addAll(twoqueues.get(activeQueue));
+    	flipQueues();
+    	twoqueues.get(inactiveQueue).clear();
+    }
+    public type Pop() throws Exception{
+    	if(twoqueues.get(activeQueue).size() < 1) throw new Exception("Cannot pop empty stack");
+    	    	
+        return twoqueues.get(activeQueue).poll();
+    }
+    private void flipQueues(){
+    	activeQueue = inactiveQueue;
+    	inactiveQueue = (inactiveQueue + 1) % 2;
+    }
+
+    /** Accessors **/
+    public type Peek(){
+        return twoqueues.get(activeQueue).peek();
+    }
+    public int Size(){
+        return twoqueues.get(activeQueue).size();
+    }
+    public boolean isEmpty(){
+        return twoqueues.get(activeQueue).size() == 0;
+    }
 
 }
