5b4c8269659a134102c82688741e8a082fc5c752
Brianna Shade <jarvis.brianna@gmail.com>
2014-05-07 19:31:14 -0700

    NodeStack implementation

---
+++ Project2/stacks/NodeStack.java
@@ -0,0 +1,48 @@
+/**
+ * "Stacks"
+ * Copyright © 2014 Brianna Shade
+ * bshade@pdx.edu
+ *
+ * NodeStack.java
+ * TODO details on this class
+ */
+package stacks;
+
+public class NodeStack<type> implements BaseStack<type> {
+
+    private Node<type> head;
+    private int size;
+
+    /** Constructor **/
+    public NodeStack(){
+        head = new Node<type>();
+        size = 0;
+    }
+
+    /** Modifiers **/
+    public void Push(type o){
+        Node<type> nextNode = head;
+        head = new Node<type>(o);
+        head.setNext(nextNode);
+        size++;
+    }
+    public type Pop() throws Exception{
+    	if(size < 1) throw new Exception("Cannot pop empty stack");
+        type value = head.getValue();
+        head = head.next();
+        size--;
+        return value;
+    }
+
+    /** Accessors **/
+    public type Peek(){
+        return head.getValue();
+    }
+    public int Size(){
+        return size;
+    }
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+}
